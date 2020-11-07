bc4e2a2ca2d932ae94830763439d1657b6537a2a
slackvariablecode <36965858+slackvariablecode@users.noreply.github.com>
2020-11-07 21:54:50 +0000

    MVP for 6 Nimmt
    Who care's about styling?

--- trackntrace.js
+++ trackntrace.js
@@ -1,25 +1,32 @@
-var data = [
-    "item1",
-    "item2",
-    "item3"
-]
-
 $(document).ready(function() {
     function createItem(value) {
-        var itemMarkup = "<div class='item' >" + value + "</div>";
+        var itemMarkup = "<div class='item col-sm-1' style='display: none' >" + value + "</div>";
         $("#itemBucket").append(itemMarkup);
+    }
+
+    function showAllHiddenItems()
+    {
+        $(".item").show('slow');
     }
     
     function popItem() {
-        $(this).remove();
+        $(this).fadeOut('slow');
+    }
+
+    function generateData() {
+        var playerCount = $("#numOfPlayers").val();
+        var cardCount = playerCount * 10 + 4;
+        return data = Array.from({length: cardCount}, (_, i) => (i + 1).toString());
     }
     
     function resetBucket() {
         $("#itemBucket").empty();
+        var data = generateData();
         for (item of data) {
             createItem(item);
         }        
         $(".item").click(popItem)
+        showAllHiddenItems();
     }
     
     $("#resetButton").click(resetBucket);
